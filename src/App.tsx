24dcb53361138a050471d43e085e4534133070b2
André Schreck <andre.schreck@gmail.com>
2021-10-27 14:13:49 +0200

    finished slides

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { Deck, Slide, Heading, CodePane, OrderedList, ListItem, UnorderedList, Appear, Text, Link, Notes, Image } from 'spectacle';
+import { Deck, Slide, Heading, CodePane, OrderedList, ListItem, UnorderedList, Appear, Text, Link, Notes, Image, FlexBox } from 'spectacle';
 import singleRequest from './single-request.png';
+import reactiveRequest from './single-request-reactive.png';
 import withLoad from './with-load.png';
 
 enum Item {
@@ -10,11 +11,23 @@ enum Item {
 
 interface Order {
     item: Item;
+    payed: boolean;
+    price: number;
+    links: object;
 }
 
 function App() {
 
     const [orders, setOrders] = useState([]);
+
+    const getOrders = () => fetch('http://localhost:8080/api/orders', {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json'
+            }
+        })
+        .then(ordersResponse => ordersResponse.json())
+        .then(setOrders);
 
     const addOrder = () => {
         fetch('http://localhost:8080/api/orders', {
@@ -29,30 +42,47 @@ function App() {
         })
         .then(response => {
             if (response.status === 201) {
-                fetch('http://localhost:8080/api/orders', {
-                    method: 'GET',
-                    headers: {
-                        Accept: 'application/json'
-                    }
-                })
-                .then(ordersResponse => ordersResponse.json())
-                .then(setOrders);
+                getOrders();
             } else {
                 throw new Error('failed to create order');
             }
-        })
-    }
+        });
+    };
 
-    const listItems = orders.map((order: Order) => order.item).map((item, index) => <ListItem key={index}>{item}</ListItem>)
+    const payOrder = (order: Order) => {
+        const link = order.links['payment'] as string;
+        fetch(`http://localhost:8080${link}`, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json'
+            }
+        })
+        .then(response => {
+            if (response.status === 200) {
+                getOrders();
+            } else {
+                throw new Error('failed to create order');
+            }
+        });
+    };
+
+    const listItems = orders
+        .map((order: Order, index) =>
+            <ListItem key={index}>
+                {order.item} { order.payed ? order.price : <Link onClick={() => payOrder(order)}>Pay</Link> }
+            </ListItem>
+        );
 
     return (
         <Deck>
             <Slide>
-                <Heading>Reactive Spring</Heading>
+                <FlexBox height="100%" flexDirection="column">
+                    <Heading>Reactive Spring</Heading>
+                </FlexBox>
             </Slide>
-            <Slide>
+            {/* <Slide>
                 <Heading>Hallo</Heading>
-            </Slide>
+            </Slide> */}
             <Slide>
                 <Heading>Spring MVC Recap</Heading>
                 <Appear>
@@ -114,12 +144,12 @@ function App() {
                 </Notes>
             </Slide>
             <Slide>
-                <Heading>Und dieses Reactive kann das jetzt besser?</Heading>
+                <Heading>Und mit reactive?</Heading>
                 <Appear>
-                    <Text>Ja, aber das malen wir nochmal auf</Text>
+                    <Text>Das malen wir nochmal auf</Text>
                 </Appear>
                 <Appear>
-                    <Text>Das Bild macht hier jetzt keinen Sinn, weil das Resultat das gleiche ist, nur der Weg ein anderer.</Text>
+                   <Image src={reactiveRequest} width={1024} />
                 </Appear>
             </Slide>
             <Slide>
@@ -153,12 +183,63 @@ function App() {
             </Slide>
             <Slide>
                 <Heading>Mono und Flux</Heading>
-                <Text>Implementierungen von Publisher</Text>
-                <Text>API ähnlich wie Optional und Stream</Text>
+                <Appear>
+                    <Text>Implementierungen von Publisher</Text>
+                    <CodePane language="java">
+                        {`
+                        package org.reactivestreams;
+                        
+                        public interface Publisher<T> {
+                            public void subscribe(Subscriber<? super T> s);
+                        }
+                        `}
+                    </CodePane>
+                </Appear>
+                <Appear>
+                    <Text>Publisher publizieren Daten, Subscriber melden sich am Publisher an um die Daten zu bekommen</Text>
+                </Appear>
                 <Notes>
                     <ul>
+                        <li>Interfaces aus der reactive streams initiative</li>
                         <li>Observer Pattern</li>
+                        <li>Daten, die asynchron erhalten wurden (z.B. aus einer DB), werden über den Publisher publiziert</li>
                     </ul>
+                </Notes>
+            </Slide>
+            <Slide>
+                <Heading>Mono und Flux</Heading>
+                <Appear>
+                    <CodePane language="java">
+                        {`
+                        package org.reactivestreams;
+                        
+                        public interface Subscriber<T> {
+                            public void onSubscribe(Subscription s);
+                            public void onNext(T t);
+                            public void onError(Throwable t);
+                            public void onComplete();
+                        }
+                        `}
+                    </CodePane>
+                </Appear>
+                <Notes>
+                    <ul>
+                        <li>Das schöne, mit dem subscriben muss man sich so gut wie nicht beschäftigen</li>
+                    </ul>
+                </Notes>
+            </Slide>
+            <Slide>
+                <Heading>Mono und Flux</Heading>
+                <Appear>
+                    <Text>Mono: Ein Publisher, der einen Wert publiziert</Text>
+                </Appear>
+                <Appear>
+                    <Text>Flux: Ein Publisher, der mehrere Werte publiziert</Text>
+                </Appear>
+                <Appear>
+                    <Text>Die API ähnelt sehr stark dem Optional bzw. Streams</Text>
+                </Appear>
+                <Notes>
                 </Notes>
             </Slide>
             <Slide>
@@ -168,40 +249,50 @@ function App() {
                     <CodePane language="java">
                         {`
                         return eventRepository.findAll()
-                            .map(Event::getData)       // Jedes Event aus der DB wird auf seine Daten gemappt
-                            .reduce(new Order(), ());  // Die Daten aller Events werden in eine Order reduced
+                            .map(Event::getData)                           // Jedes Event aus der DB wird auf seine Daten gemappt
+                            .reduce(new Order(), (o, e) -> e.process(o));  // Die Daten aller Events werden in eine Order reduced
                         `}
                     </CodePane>
                 </Appear>
                 <Appear>
-                    <Text>Aufruf einer externen Applikation</Text>
+                    <Text>Aufruf einer externen Applikation per HTTP</Text>
                     <CodePane language="java">
                         {`
                         return webClient
                             .get()
                             .uri("/api/products")
                             .accept(MediaType.APPLICATION_JSON)
-                            .exchangeToMono(clientResponse -> clientResponse.bodyToMono(PaymentResponse.class))
-                            .filter(Product::isInStock);
+                            .exchangeToFlux(clientResponse -> clientResponse.bodyToFlux(Product.class))
+                            .filter(Product::isInStock);  // Nur vorrätige Produkte kommen beim Subscriber an
                         `}
                     </CodePane>
                 </Appear>
             </Slide>
             <Slide>
                 <Heading>Unser Controller vom Anfang in Webflux</Heading>
-                <CodePane language="java">
-                    {`
-                    @RestController
-                    @RequestMapping("/api/orders")
-                    class OrderController {
+                <Appear>
+                    <CodePane language="java">
+                        {`
+                        @RestController
+                        @RequestMapping("/api/orders")
+                        class OrderController {
 
-                        @GetMapping(produces = "application/json")
-                        public Flux<OrderDTO> getOrders() {
-                            return orderService.getOrders();
+                            @GetMapping(produces = "application/json")
+                            public Flux<OrderDTO> getOrders() {
+                                return orderService.getOrders();
+                            }
                         }
-                    }
-                    `}
-                </CodePane>
+                        `}
+                    </CodePane>
+                </Appear>
+                <Appear>
+                    <Text>Um das subscribe kümmert sich der Servlet Container</Text>
+                </Appear>
+                <Notes>
+                    <ul>
+                        <li>Ich muss nur implementieren, was mit den Daten passieren soll.</li>
+                    </ul>
+                </Notes>
             </Slide>
             <Slide>
                 <Heading>Übung</Heading>
